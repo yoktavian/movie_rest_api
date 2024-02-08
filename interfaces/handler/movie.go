d90048f001954845b2679ecea43f355a73d129e7
Yuda Oktavian <yoktavian17@gmail.com>
2024-02-08 10:37:04 +0700

    fix: unused err

--- interfaces/handler/movie.go
+++ interfaces/handler/movie.go
@@ -45,8 +45,8 @@ func (s *movieHandler) Create(c *gin.Context) {
 func (s *movieHandler) Read(c *gin.Context) {
 	limitQuery := c.Query("limit")
 	offsetQuery := c.Query("offset")
-	limit, err := strconv.Atoi(limitQuery)
-	offset, err := strconv.Atoi(offsetQuery)
+	limit, _ := strconv.Atoi(limitQuery)
+	offset, _ := strconv.Atoi(offsetQuery)
 
 	// use base, no need to differentiate based on movie type
 	movies, err := s.usecases[""].Read(limit, offset)
